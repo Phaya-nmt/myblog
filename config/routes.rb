017194c9accd4e6d0f99e337d0bfab67d2cfe27d
Phaya-nmt <tipherreth@hotmail.com>
2016-01-08 02:37:30 +0000

    Edit posts files

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :users
+  resources :posts
+  # get 'pages/index'
+  root 'posts#index'
+
+  get 'pages/show'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
